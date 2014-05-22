62f90658a34c80af7908a05cb820253decedaab7
crynobone <crynobone@gmail.com>
2014-05-22 15:19:07 +0800

    Add a Facade for XML Document. Closes #2
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Parser/Xml/Facade.php
@@ -0,0 +1,14 @@
+<?php namespace Orchestra\Parser\Xml;
+
+class Facade extends \Illuminate\Support\Facades\Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'orchestra.parser.xml';
+    }
+}
